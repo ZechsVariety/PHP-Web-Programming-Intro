d9de770712870cde147c31c687f68ed66112d372
Zecheriah <98231952+ZechsVariety@users.noreply.github.com>
2026-02-14 13:54:12 -0500

    project phase1: initial form
    pretty simple. date will be determined automatically using sql

---
+++ assignments/project/phase_one/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Awesome Blog - Create Post</title>
+    </head>
+
+    <body>
+        <header>
+            <h1>Awesome Blog</h1>
+            <h3>Create a Post</h3>
+        </header>
+
+        <main>
+            <form action="process.php" method="post">
+                <label for="postTitle">Post Title</label>
+                <input type="text" id="postTitle" name="postTitle">
+
+                <label for="postContent">Content</label>
+                <textarea id="postContent" name="postContent" placeholder="Enter Text Here..."></textarea>
+
+                <label for="mainTag">Main Tag</label>
+                <input type="text" id="mainTag" name="mainTag" placeholder="Ex: #art">
+
+                <button type="submit">Post</button>
+            </form>
+
+            <p>
+                <a href="subscribers.php">View Blog Posts</a>
+            </p>
+        </main>
+    </body>
+</html>
