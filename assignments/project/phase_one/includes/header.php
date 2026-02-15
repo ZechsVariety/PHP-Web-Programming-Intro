d7ad67fde818ebf70f5cd273e364c3d221aa9ec2
Zecheriah <98231952+ZechsVariety@users.noreply.github.com>
2026-02-15 14:39:59 -0500

    phase1: index bootstrap
    im just doing relatively basic stuff since this isnt directly on the rubric
    
    also
    - centered the header

--- assignments/project/phase_one/includes/header.php
+++ assignments/project/phase_one/includes/header.php
@@ -1,4 +1,4 @@
-<header>
-    <h1>Awesome Blog</h1>
+<header style="text-align: center;">
+    <h1 class="mt-3">Awesome Blog</h1>
     <p>By: Zecheriah</p>
 </header>
--- assignments/project/phase_one/index.php
+++ assignments/project/phase_one/index.php
@@ -4,29 +4,43 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Awesome Blog - Create Post</title>
+
+        <!-- bootstrap css -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     </head>
 
     <body>
         <?php require "includes/header.php"; ?>
     
-        <main>
-            <h3>Create a Post</h3>
+        <!-- 5 padding -->
+        <main class="container">
+            <div class="container border mb-3">
+                <h3 class="m-3">Create a Post</h3>
 
-            <form action="process.php" method="post">
-                <label for="title">Post Title</label>
-                <input type="text" id="title" name="title" maxlength="100" required>
+                <form action="process.php" method="post">
+                    <!-- mb is margin bottom -->
+                    <div class="input-group mb-3">
+                        <label for="title" class="input-group-text">Post Title</label>
+                        <input type="text" id="title" name="title" maxlength="100" required class="form-control">
+                    </div>
 
-                <label for="content">Content</label>
-                <textarea id="content" name="content" maxlength="1000" required placeholder="Enter Text Here..."></textarea>
+                    <div class="input-group mb-3">
+                        <label for="content" class="input-group-text">Content</label>
+                        <textarea id="content" name="content" maxlength="1000" required placeholder="Enter Text Here..." class="form-control"></textarea>
+                    </div>
 
-                <label for="mainTag">Main Tag</label>
-                <input type="text" id="mainTag" name="mainTag" maxlength="100" required placeholder="Ex: #art">
+                    <div class="input-group mb-3">
+                        <label for="mainTag" class="input-group-text">Main Tag</label>
+                        <input type="text" id="mainTag" name="mainTag" maxlength="100" required placeholder="Ex: #art" class="form-control">
+                    </div>
 
-                <button type="submit">Post</button>
-            </form>
+                    <button type="submit" class="btn btn-primary mb-3">Post</button>
+                </form>
+            </div>
 
             <p>
-                <a href="blog.php">View Blog Posts</a>
+                <!-- bootstrap documentation says to give a's that act as buttons "role="button"" for the sake of screenreaders and stuff https://getbootstrap.com/docs/5.3/components/buttons/#button-tags -->
+                <a href="blog.php" role="button" class="btn btn-secondary btn-sm mb-3">View Blog Posts</a>
             </p>
         </main>
     </body>
